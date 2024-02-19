8c2c968d2e5e8fb16c98e856b87b092e100d60ad
yukitamurang <kitamura-yu816@g.ecc.u-tokyo.ac.jp>
2024-02-19 19:05:34 +0900

    finished tutorial

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,20 @@ class Post(models.Model): # models.Modelで、データベースに保存すべ�
 
     def __str__(self):
         return self.title
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+    
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments') # Postモデルの中からコメントにアクセスできるようにする
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- mysite/settings.py
+++ mysite/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
 }
 
 
+LOGIN_REDIRECT_URL = '/'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
